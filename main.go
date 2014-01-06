c3f245b3dd2c277b924bd44f82b1fde6267c1fb9
atma <atma@ubuntu.(none)>
2014-01-07 00:37:23 +1030

    make an exectuable to reverse complement seqs

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"code.google.com/p/biogo/alphabet"
+	"code.google.com/p/biogo/io/seqio"
+	"code.google.com/p/biogo/io/seqio/fasta"
+	"code.google.com/p/biogo/seq/linear"
+	"fmt"
+	"os"
+)
+
+//Usage: RevComp inputFileName outputFileName
+func main() {
+
+	inputFileName := os.Args[1]
+	outputFileName := os.Args[2]
+
+	// open inputFile in read-only mode
+	inputFile, inputError := os.Open(inputFileName)
+	if inputError != nil {
+		fmt.Println("An error occurred opening the input file.")
+		return
+	}
+	defer inputFile.Close()
+
+	// open outputFile in write-only mode
+	outputFile, outputError := os.Create(outputFileName)
+	if outputError != nil {
+		fmt.Println("An error occurred with file creation.")
+		return
+	}
+	defer outputFile.Close()
+
+	outputWriter := bufio.NewWriter(outputFile)
+
+	sc := seqio.NewScanner(fasta.NewReader(inputFile, linear.NewSeq("", nil, alphabet.DNA)))
+	for sc.Next() {
+		s := sc.Seq().(*linear.Seq)
+		for i := range s.Seq {
+			s.Seq[i] &^= ' '
+		}
+		// indicate that this is the reverse-complemented sequence
+		s.ID = s.ID + ".rev"
+		// write header
+		outputWriter.WriteString(">" + s.ID + "\n")
+		// reverse complement the sequence
+		s.RevComp()
+		// write FASTA sequence
+		outputWriter.WriteString(s.String() + "\n")
+	}
+	if sc.Error() != nil {
+		panic(sc.Error())
+	}
+
+	// write the buffer completely to the file
+	outputWriter.Flush()
+}
